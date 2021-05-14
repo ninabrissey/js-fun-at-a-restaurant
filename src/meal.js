53ab480627aff7b1b11918c9fed448b292e201fe
Nina Brissey <nbrissey@gmail.com>
2021-05-14 00:53:00 -0700

    Pass all tests meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,62 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
 }
+
+function createMenuItem(menuItemName) {
+  var menuItem = {
+    name: menuItemName,
+    price: 10.99,
+    type: 'breakfast'
+  };
+  return menuItem;
+};
+
+//alt
+
+function createMenuItem(menuItemName, price, type) {
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
+
+
+
+function addIngredients(newIngredient, ingredientsArray) {
+  for (var i = 0; i < ingredientsArray.length; i++) {
+    if (ingredientsArray[i] === newIngredient) {
+      return;
+    }
+  }
+  ingredientsArray.push(newIngredient);
+};
+
+
+function formatPrice(unformattedPrice) {
+  return `$${unformattedPrice}`;
+};
+
+function decreasePrice(fullPriceItem){
+  return fullPriceItem * .9;
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
